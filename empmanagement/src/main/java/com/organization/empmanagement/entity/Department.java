4a89d4f1147fb2cec99160a2815d5d0731f6716e
karthikeyan.k <k.karthikeyankcbe@gmail.com>
2025-03-05 12:21:20 +0530

    First Commit

--- empmanagement/src/main/java/com/organization/empmanagement/entity/Department.java
+++ empmanagement/src/main/java/com/organization/empmanagement/entity/Department.java
@@ -1,9 +1,18 @@
 package com.organization.empmanagement.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String code;
 
